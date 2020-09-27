2ac23d5edb54b7b874a8c5c656c48baea2a4a38c
Azat <razatr@yandex.ru>
2020-09-27 16:33:40 +0300

    added accordeon

---
+++ src/decorators/accordion.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+const accordion = OriginalComponent =>
+    class DecoratedComponent extends Component {
+        state = {
+            openItemId: null
+        };
+
+        render() {
+            return (
+                <OriginalComponent
+                    {...this.props}
+                    {...this.state}
+                    toggleOpenItem={this.toggleOpenItem}
+                />
+            );
+        }
+
+        toggleOpenItem = id => {
+            this.setState({
+                openItemId: id
+            });
+        };
+    };
+
+export { accordion };
