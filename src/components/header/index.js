b10cdc05f84cc41d1d1641a451a9f19beb401ff4
Azat <razatr@yandex.ru>
2020-10-01 09:29:01 +0300

    added menu design

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,19 +2,12 @@ import React, { Component } from 'react'
 import { Grid, Button } from '@material-ui/core'
 import { withTheme } from '@material-ui/core/styles'
 import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined'
+import PrimarySearchAppBar from './menu'
 
 class Header extends Component {
     render() {
         return (
-            <Grid container style={{ height: '50px' }}>
-                <Grid className='menu' xs>
-                </Grid>
-                <Grid className='cart-badge' xs={1}>
-                    <Button>
-                        <ShoppingCartOutlinedIcon/>
-                    </Button>
-                </Grid>
-            </Grid>
+                <PrimarySearchAppBar/>
         )
     }
 }
