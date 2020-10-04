4075f43ecc45e1cc714eb91406842012350ac5ae
Azat <razatr@yandex.ru>
2020-10-04 21:27:04 +0300

    middleware random-id was added

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware } from 'redux'
-import logger from '../middlewares'
+import logger from '../middlewares/logger'
+import randomId from '../middlewares/randomId'
 import reducer from '../reducers'
 
-const enhancer = applyMiddleware(logger)
+const enhancer = applyMiddleware(randomId, logger)
 
 const store = createStore(reducer, enhancer)
 
