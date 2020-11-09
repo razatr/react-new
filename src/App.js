2275308cb4206875c6136bce5105a85612dd671a
Azat <razatr@yandex.ru>
2020-11-09 21:28:59 +0300

    fixed bug in workflow

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import Header from './components/header'
 import OrderList from './components/order-list'
 import { BrowserRouter, Route, Redirect } from 'react-router-dom'
 import RestaurantPage from './components/routes/restaurantsPage'
-import {rootPath} from './constants'
+import { rootPath } from './constants'
 
 function App() {
-    return < BrowserRouter>
-        <Route exact path={`${rootPath}`}>
-            <Redirect to="/restaurants" />
+    return <BrowserRouter>
+        <Route exact path={ `${ rootPath }` }>
+            <Redirect to={ `${ rootPath }restaurants` } />
         </Route>
         <Header />
-        <Route path={ `${rootPath}restaurants` } component={ RestaurantPage } />
-        <Route path={ `${rootPath}user-form` } component={ UserForm } />
-        <Route path={ `${rootPath}order-list` } component={ OrderList } />
+        <Route path={ `${ rootPath }restaurants` } component={ RestaurantPage } />
+        <Route path={ `${ rootPath }user-form` } component={ UserForm } />
+        <Route path={ `${ rootPath }order-list` } component={ OrderList } />
     </BrowserRouter>
 }
 
